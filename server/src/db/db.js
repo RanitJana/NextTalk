f13bba2a5977af3cc3cf829d108ebcddf03c63df
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2025-04-19 01:02:53 +0530

    prettier

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 import { _env, DB_NAME } from "../constant.js";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(
-            `${_env.MONGODB_URI}/${DB_NAME}`
-        );
-        console.log("MongoDB connected ... ");
-        console.log(`\n DB host : ${connectionInstance.connection.host}\n`);
-    } catch (error) {
-        console.log(`MongoDb connection-fail error: , ${error}`);
-        process.exit(1);
-    }
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${_env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log("MongoDB connected ... ");
+    console.log(`\n DB host : ${connectionInstance.connection.host}\n`);
+  } catch (error) {
+    console.log(`MongoDb connection-fail error: , ${error}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
