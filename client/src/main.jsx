00d25328cd960d9840772835c55b83ee6602187f
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2025-04-20 01:09:05 +0530

    import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import SocketProvider from "./context/SocketProvider.jsx";
+import ChatProvider from "./context/ChatProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
