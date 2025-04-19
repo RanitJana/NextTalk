287ea48efbcdf6cb1509d1caa12bd9e4729c32c3
mdsalmanfarsee <far90986@gmail.com>
2025-04-19 17:32:03 +0530

    update useAuthStore

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -88,5 +88,24 @@ export const useAuthStore = create((set, get) => ({
         }
     },
 
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.post('/updateProfile', data);
+            set({ authUser: res.data });
+            localStorage.setItem('authUser', JSON.stringify(res.data));
+            toast.success(res.data.message);
+
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log('error in updateProfile:', error);
+
+        }
+        finally {
+            set({ isUpdatingProfile: false });
+        }
+    }
+
 
 }));
