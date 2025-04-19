698a35b524a553fbf6f161a881bd5ab1ed3f1353
Ranit jana <ranitjana100@gmail.com>
2025-04-20 03:32:46 +0530

    feat: pfp & name in chat

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const ChatBox = () => {
   return (
-    <div className="flex-1 overflow-y-auto p-4 space-y-2 bg-base-100">
-      {[...Array(5)].map((_, idx) => (
-        <div
-          key={idx}
-          className={`max-w-[70%] p-3 rounded-xl ${
-            idx % 2 === 0
-              ? "bg-primary text-primary-content self-end ml-auto"
-              : "bg-base-300 self-start"
-          }`}
-        >
-          <p className="text-sm">Message {idx + 1}</p>
-        </div>
-      ))}
+    <div className="relative w-full h-full overflow-y-auto">
+      <div className=" absolute top-0 left-0 he-full w-full flex-1 overflow-y-auto p-4 space-y-2 bg-base-100">
+        {[...Array(50)].map((_, idx) => (
+          <div
+            key={idx}
+            className={`max-w-[75%] w-fit p-3 rounded-xl ${
+              idx % 2 === 0
+                ? "bg-primary text-primary-content self-end ml-auto"
+                : "bg-base-300 self-start"
+            }`}
+          >
+            <p className="text-sm break-words text-pretty">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptates iure esse qui quam dicta officiis! Laboriosam labore vel aut ipsam maiores facere, saepe cupiditate consequuntur explicabo perspiciatis aspernatur similique?</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
