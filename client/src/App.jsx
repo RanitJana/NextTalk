b34c32186245a227b8ec765ecbc7812a438c0dd2
mdsalmanfarsee <far90986@gmail.com>
2025-04-19 13:51:46 +0530

    setup axios Instance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,40 @@
+import React, { useEffect } from 'react'
+import { Loader } from 'lucide-react'
+
+
+
 import { useThemeStore } from './store/useThemeStore.js'
+import { useAuthStore } from './store/useAuthStore.js'
+
+
+import Navbar from './components/Navbar.jsx'
+
+
 import SettingPage from './pages/SettingPage'
 
 
 function App() {
   const { theme } = useThemeStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+
+  // useEffect(() => {
+  //   checkAuth();
+  // }, [checkAuth])
+
+  //console.log({ authUser }); //for debug purposes
+  // if (isCheckingAuth && !authUser) {
+  //   return (
+  //     <div className='flex justify-center items-center h-screen' data-theme={theme}>
+  //       <Loader className='size-10 animate-spin bg-base-100' />
+  //     </div>
+  //   )
+  // }
+
 
   return (
     <div data-theme={theme}>
+      {/* <Navbar /> */}
       <SettingPage />
     </div>
   )
