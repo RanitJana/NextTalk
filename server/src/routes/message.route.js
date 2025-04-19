10316bdad8d9f0bf37e263d3cec877c180e0487c
Ranit Jana <ranitjana100@gmail.com>
2025-04-19 12:47:02 +0530

    feat:attachment upload

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -2,8 +2,33 @@ import express from "express";
 import verify from "../middlewares/verify.middleware.js";
 import { postMessage } from "../controllers/message.controller.js";
 
+import upload from "../utils/multer.js";
+
 const router = express.Router();
 
-router.post("/", verify, postMessage);
+const cpUpload = upload.fields([
+  {
+    name: "image",
+    maxCount: 5,
+  },
+  {
+    name: "video",
+    maxCount: 2,
+  },
+  {
+    name: "audio",
+    maxCount: 5,
+  },
+  {
+    name: "document",
+    maxCount: 5,
+  },
+  {
+    name: "gif",
+    maxCount: 5,
+  },
+]);
+
+router.post("/", verify, cpUpload, postMessage);
 
 export default router;
---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import { _env } from "../constant.js";
+
+cloudinary.config({
+  cloud_name: _env.CLOUD_NAME,
+  api_key: _env.API_KEY,
+  api_secret: _env.API_SECRET,
+});
+
+const uploadFile = async function (filePath, name) {
+  const uploadResult = await cloudinary.uploader
+    .upload(filePath, { public_id: `${name}` })
+    .catch((err) => {
+      console.log(err);
+    });
+  return uploadResult;
+};
+
+const deleteFile = async (publicId) => {
+  return await cloudinary.uploader.destroy(publicId, (err, res) => {
+    if (err) console.log(err);
+  });
+};
+
+export { uploadFile, deleteFile };
