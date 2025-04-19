464f9671b6a53e6fd8b5f90bd693fcd5113eb3b9
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2025-04-19 17:05:05 +0530

    check for same email

--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import verify from '../middlewares/verify.middleware.js';
 import { addToGroup, createGroupChat, createOneToOneChat, fetchAllChats, removeFromGroup } from '../controllers/chat.controller.js';
+import upload from '../utils/multer.js';
 
 const router = express.Router();
 
 router.route("/").get(verify, fetchAllChats);                                   // get all chats of user
 router.route("/oneToOneChat").post(verify, createOneToOneChat);                 // get one to one chat
-router.route("/group").post(verify, createGroupChat);                           // create group chat
+router.route("/group").post(verify, upload.single("groupIcon"), createGroupChat);                           // create group chat
 router.route("/groupadd").put(verify, addToGroup);                              // add user to group             
 router.route("/groupremove").put(verify, removeFromGroup);                      // remove user from group
 
