e715bf118561521de687fd951f6321168ec0295a
Ranit jana <ranitjana100@gmail.com>
2025-04-20 13:38:48 +0530

    feat:update vercel.json

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/api/v1";
+const url = "https://nexttalk.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL: url,
